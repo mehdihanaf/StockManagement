6277f2c7475879ead577d7d6624b6eeec877c998
Mehdi <medios1596@gmail.com>
2024-09-16 22:52:45 +0100

    "create class category controller"

---
+++ src/main/java/com/stock/controllers/CategoryController.java
@@ -0,0 +1,10 @@
+package com.stock.controllers;
+
+import com.stock.api.controller.CategoriesApi;
+import com.stock.api.controller.ProductsApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryController implements CategoriesApi {
+
+}
