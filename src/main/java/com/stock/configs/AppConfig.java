da74d23acf3c51538d5dec795352abcc8b01dc85
muhsiine <muhsiine@gmail.com>
2024-09-29 17:41:46 +0100

    unify configuration.

--- src/main/java/com/stock/configs/AppConfig.java
+++ src/main/java/com/stock/configs/AppConfig.java
@@ -3,6 +3,7 @@ package com.stock.configs;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 public class AppConfig {
