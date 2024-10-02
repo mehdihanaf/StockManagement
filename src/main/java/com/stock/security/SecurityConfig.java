16d6601b65d378d5e9948184b260e19da1715402
ismail RZOUKI <ismailrzouki7@gmail.com>
2024-10-02 13:21:05 +0200

    fix cors

--- src/main/java/com/stock/security/SecurityConfig.java
+++ src/main/java/com/stock/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.stock.security;
 
 import com.stock.configs.JwtAuthenticationEntryPoint;
 import com.stock.configs.JwtRequestFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,24 +19,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
     private final AuthService authService;
-
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-
     private final UserDetailsService jwtUserDetailsService;
-
-    @Autowired
     private final JwtRequestFilter jwtRequestFilter;
-
 
     @Value("${allowed.origins}")
     private String allowedOrigins;
@@ -57,7 +50,6 @@ public class SecurityConfig {
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-
         authProvider.setUserDetailsService(jwtUserDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
 
@@ -71,39 +63,37 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        /*http.csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(requests -> requests
-            .requestMatchers("/api/v1/user/login").permitAll())
-            .httpBasic(withDefaults())
-            .sessionManagement(management -> management
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .addFilterBefore(new AuthFilter(authService), UsernamePasswordAuthenticationFilter.class);
-        http.httpBasic(AbstractHttpConfigurer::disable);*/
-
-        /*http.authorizeRequests(authorizeRequests -> authorizeRequests.requestMatchers("/api/v1/user/login").permitAll()
-                        .anyRequest()
-                        .authenticated())
-                .httpBasic(withDefaults())
-                .formLogin(withDefaults())
-                .csrf(AbstractHttpConfigurer::disable);
-        return http.build();*/
-
         return http
-                .cors(cors -> cors.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement((session) ->
+                .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .authorizeHttpRequests(authorize -> {
-                            authorize
-                                    .requestMatchers("/api/v1/user/login/**").permitAll()
-                                    .anyRequest().authenticated();
-                        }
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/v1/user/login/**").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        var allowedOrigins = this.allowedOrigins.split(",");
+        final var configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(allowedOrigins));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowCredentials(true);
+        // the below three lines will add the relevant CORS response headers
+        for(String link: allowedOrigins) {
+            configuration.addAllowedOrigin(link);
+        }
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        final var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
@@ -111,18 +101,6 @@ public class SecurityConfig {
         return (web) -> web.ignoring().requestMatchers("/js/**",
                 "/images/**", "/v3/api-docs", "/configuration/ui", "/swagger-resources/**",
                 "/swagger-ui.html", "/webjars/**", "/swagger-ui/**", "/swagger-ui/**", "/v3/api-docs/**");
-    }
-
-    // global
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins(allowedOrigins).allowedMethods("GET", "POST", "PUT",
-                        "DELETE");
-            }
-        };
     }
 
 }
