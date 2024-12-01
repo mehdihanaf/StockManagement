2de5c00c1ef7b3e7f91df501c6516a9ccb67852b
medisensei <medios1596@gmail.com>
2024-12-01 17:20:56 +0100

    cards statistics V2

--- src/main/java/com/stock/services/impl/DashboardServiceImpl.java
+++ src/main/java/com/stock/services/impl/DashboardServiceImpl.java
@@ -11,7 +11,10 @@ import com.stock.services.IDashboardService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -37,15 +40,16 @@ public class DashboardServiceImpl implements IDashboardService {
 
         DashboardDTO dashboardDTO = new DashboardDTO();
         List<Sale> sales = saleRepository.findAll();
-
+        Month currentMonth = LocalDate.now().getMonth();
         Long countCategories = categoryRepository.count();
         Long countProducts = productRepository.count();
         Long countSales = saleRepository.count();
         Double profits = sales.stream()
+                .filter(sale -> sale.getSaleDate().getMonth() == currentMonth)
                 .mapToDouble(sale ->
                         (sale.getProduct().getUnitSellPrice() - sale.getProduct().getUnitBuyPrice())
                                 * sale.getSaleQuantity()
-                                - sale.getDiscount()
+                                - Optional.ofNullable(sale.getDiscount()).orElse(0.0)
                 )
                 .sum();
 
